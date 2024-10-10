52ebc7cee702dc7aad8e05ad3657ed1d7a8e67d4
Sahriz <johnthaiy@gmail.com>
2024-10-10 17:25:11 +0200

    Variables
    Camera settings easier to tweak.

--- TNCG15/src/main.cpp
+++ TNCG15/src/main.cpp
@@ -15,7 +15,11 @@ int main()
 	std::cout << "Hello CMake." << std::endl;
 	const int imageWidth = 800;
 	const int imageHeight = 800;
-	Camera mainCamera(glm::vec3(0,0,0), glm::vec3(1,0,0), glm::vec3(0,0,1), 3.14159f/4.0f,(float)imageWidth/imageHeight); //TODO: fixa så att aspectRatio och fov beräknas i constructorn istället
+	const glm::vec3 cam_pos(0, 0, 0);
+	const glm::vec3 cam_up(0,0,1);
+	const glm::vec3 cam_forward(1,0,0);
+	const float cam_fov = 3.14159f / 4.0f;
+	Camera mainCamera(cam_pos, cam_forward, cam_up, cam_fov,(float)imageWidth/imageHeight); //TODO: fixa så att aspectRatio och fov beräknas i constructorn istället
 
 	std::vector<Polygon*> scene;
 
@@ -41,7 +45,7 @@ int main()
 	//bottom
 	Rectangle* wall1 = new Rectangle(glm::vec3(-3, 0, 5), glm::vec3(0, -6, 5), glm::vec3(0, -6, -5), glm::vec3(-3, 0, -5), glm::vec3(0, 255, 0)); 
 	Rectangle* wall2 = new Rectangle(glm::vec3(0, -6, 5), glm::vec3(10, -6, 5), glm::vec3(10, -6, -5), glm::vec3(0, -6, -5), glm::vec3(255, 0, 0)); 
-	Rectangle* wall3 = new Rectangle(glm::vec3(10, -6, 5), glm::vec3(13, 0, 5), glm::vec3(13, 0, -5), glm::vec3(10, -6, -5), glm::vec3(0, 255, 0)); //Målvägg höger //looking at
+	Rectangle* wall3 = new Rectangle(glm::vec3(10, -6, 5), glm::vec3(13, 0, 5), glm::vec3(13, 0, -5), glm::vec3(10, -6, -5), glm::vec3(255, 255, 0)); //Målvägg höger //looking at
 	scene.push_back(wall1);
 	scene.push_back(wall2);
 	scene.push_back(wall3);
@@ -49,7 +53,7 @@ int main()
 	//top
 	Rectangle* wall4 = new Rectangle(glm::vec3(-3, 0, 5), glm::vec3(-3, 0, -5), glm::vec3(0, 6, -5), glm::vec3(0, 6, 5), glm::vec3(0, 0, 0)); 
 	Rectangle* wall5 = new Rectangle(glm::vec3(0, 6, 5), glm::vec3(0, 6, -5), glm::vec3(10, 6, -5), glm::vec3(10, 6, 5), glm::vec3(0, 255, 0)); 
-	Rectangle* wall6 = new Rectangle(glm::vec3(10, 6, 5), glm::vec3(10, 6, -5), glm::vec3(13, 0, -5), glm::vec3(13, 0, 5), glm::vec3(255, 0, 0)); //Målvägg vänster //looking at
+	Rectangle* wall6 = new Rectangle(glm::vec3(10, 6, 5), glm::vec3(10, 6, -5), glm::vec3(13, 0, -5), glm::vec3(13, 0, 5), glm::vec3(255, 0, 255)); //Målvägg vänster //looking at
 	scene.push_back(wall4);
 	scene.push_back(wall5);
 	scene.push_back(wall6);
